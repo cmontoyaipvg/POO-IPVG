ba63abcddb969537717c01255c23c51cb60e8fb4
cmontoyaipvg <claudio.montoya1@virginiogomez.cl>
2022-08-22 22:28:57 -0400

    clase computador metodos de acceso

--- Clase2/Program.cs
+++ Clase2/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Modelos;
 namespace Clase2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Computador pc = new Computador(2500,1000);
+            Computador pc2 = new Computador(4000,800);
+           
+
+            Console.WriteLine(pc.ram+" "+pc.marca+" "+pc.disco+" "+pc.GetModelo());
+            pc2.SetModelo("hpX turbo");
+            Console.WriteLine(pc2.ram + " " + pc2.marca + " " + pc2.disco+" "+pc2.GetModelo());
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Modelos/Computador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Computador
+    {
+        public string cpu;
+        public int ram;
+        private string modelo;
+        public int disco;
+        public string marca;
+        private int precio;
+
+        public Computador(int cantidadRam, int cantidadDisco)
+        {
+            ram = cantidadRam;
+            marca = "HP";
+            cpu = "I7 89788";
+            disco = cantidadDisco;
+            modelo = "Pavilion";
+        }
+
+        public string GetModelo()
+        {
+            return modelo;
+        }
+
+        public void SetModelo(string modelo) {
+            this.modelo = modelo;
+        }
+    }
+}
