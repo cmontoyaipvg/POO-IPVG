d34fbd0054598e940ba90349b959a8c286f00f4b
cmontoyaipvg <claudio.montoya1@virginiogomez.cl>
2022-09-05 23:32:35 -0300

    practico

---
+++ Practico/Direccion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico
+{
+    public class Direccion
+    {
+        public string calle { get; set; }
+        public string region { get; set; }
+        public int numero { get; set; }
+        public string ciudad { get; set; }
+
+        public Direccion()
+        {
+
+        }
+
+        public Direccion(string calle, string region, int numero, string ciudad)
+        {
+            this.calle = calle;
+            this.region = region;
+            this.numero = numero;
+            this.ciudad = ciudad;
+        }
+    }
+}
---
+++ Practico/Vendedor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico
+{
+    public class Vendedor
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+
+        public Vendedor()
+        {
+
+        }
+
+        public Vendedor(string nombre, string apellido, string telefono, string email)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.telefono = telefono;
+            this.email = email;
+        }
+    }
+}
