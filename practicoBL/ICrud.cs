be426b8f23a9598fb26328ca0b875871e2412ea0
cmontoyaipvg <claudio.montoya1@virginiogomez.cl>
2022-09-14 22:40:29 -0300

    proyeto almacen

---
+++ practicoBL/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicoBL
+{
+    internal interface ICrud
+    {
+        List<object> listar(); 
+        object buscar(int id);
+        void eliminar(object o);
+        void actualizar(object o);
+        void guardar(object o);
+    }
+}
